c9668e0e85b5e81b749ef41e57ba18de0407922e
yves-roland17 <roland.tia@epitech.eu>
2026-07-21 22:29:15 +0000

    il reste  à l"herberger

--- campus_challenge/next.config.ts
+++ campus_challenge/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    remotePatterns: [
+      // si tu charges des images externes (avatars, uploads...)
+      { protocol: "https", hostname: "**.exemple.com" }
+    ]
+  },
+  experimental: {
+    // si tu utilises des Server Actions avec des payloads volumineux
+    serverActions: {
+      bodySizeLimit: "2mb"
+    }
+  }
 };
 
 export default nextConfig;
